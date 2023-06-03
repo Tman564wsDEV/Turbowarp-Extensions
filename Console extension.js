31607fcc6be71edfee2c4476d322c38f61f38fe1
Tman564wsDEV <133183000+Tman564wsDEV@users.noreply.github.com>
2023-06-03 17:04:39 -0500

    Rewrite, Console extension.js
    Made it better but more complex

--- Console extension.js
+++ Console extension.js
@@ -1,29 +1,73 @@
-(function(ext) {
-  // Block definition
-  var descriptor = {
-    blocks: [
-      [' ', 'Log %s to console', 'logToConsole', 'Script executed'],
-      [' ', 'Warn %s to console', 'warnToConsole', 'Script Warning'],
-      [' ', 'Error %s to console', 'errorToConsole', 'Error Warning']
-    ],
-    displayName: 'Console Extension'
-  };
-  
-  // Block implementation
-  ext.logToConsole = function(text) {
-    // Implement the functionality of your custom block here
-    console.log(text);
-  };
- 
-  ext.warnToConsole = function(text) {
-    console.warn(text);
-  };
-  
-  ext.errorToConsole = function(text) {
-    console.error(text);
-  };
- 
-  
-  // Register the extension
-  ScratchExtensions.register('console_extension', descriptor, ext);
-})(this);
+function getRandomExtensionID(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+class ConsoleExtension {
+  getInfo() {
+    const extensionID = getRandomExtensionID(10); // Generate a 10-character random ID
+
+    return {
+      id: extensionID,
+      name: 'Console Extension',
+      blocks: [
+        {
+          opcode: 'logToConsole',
+          blockType: 'command',
+          text: 'Log [VALUE] to console',
+          arguments: {
+            VALUE: {
+              type: 'string',
+              defaultValue: 'Script executed'
+            }
+          }
+        },
+        {
+          opcode: 'warnToConsole',
+          blockType: 'command',
+          text: 'Warn [VALUE] to console',
+          arguments: {
+            VALUE: {
+              type: 'string',
+              defaultValue: 'Script Warning'
+            }
+          }
+        },
+        {
+          opcode: 'errorToConsole',
+          blockType: 'command',
+          text: 'Error [VALUE] to console',
+          arguments: {
+            VALUE: {
+              type: 'string',
+              defaultValue: 'Error Warning'
+            }
+          }
+        }
+      ],
+      color1: '#FF8C1A',
+      color2: '#DB7C00'
+    };
+  }
+
+  logToConsole(args) {
+    console.log(args.VALUE);
+  }
+
+  warnToConsole(args) {
+    console.warn(args.VALUE);
+  }
+
+  errorToConsole(args) {
+    console.error(args.VALUE);
+  }
+}
+
+Scratch.extensions.register(new ConsoleExtension());
